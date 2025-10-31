918c4d795d38919f9015a055030b947f38193976
Takuro Kawaguchi <takawagu@c4r2s13.42tokyo.jp>
2025-10-31 11:13:50 +0900

    conflict_kaisyo

--- include/minishell.h
+++ include/minishell.h
@@ -109,6 +109,7 @@ typedef struct s_cmd
 	t_redir *redirs; // このコマンドのリダイレクト連結リスト
 	int is_builtin;  // 実行時の分岐に使うフラグ（0/1） or 別enum
 	int is_child;    //子プロセスの中かどうかの判定
+	t_shell					*sh;
 }							t_cmd;
 
 // ASTノード本体
@@ -143,6 +144,8 @@ typedef struct s_shell
 	int interactive; // 対話モードか
 	t_env					*env;
 	// 必要なら: sigフラグや一時fdの退避など
+	int						should_exit;
+	int						exit_status;
 }							t_shell;
 
 // parse用の構造体
@@ -269,5 +272,18 @@ int							builtin_env(char **argv, t_env **env);
 int							builtin_pwd(char **argv);
 int							builtin_unset(char **argv, t_env **penv);
 int							is_valid_identifier(const char *s);
+// exit
+int							str_to_ll_checked(const char *s, long long *out);
+int							builtin_exit(char **av, t_shell *sh, int is_child);
+
+// heredoc
+void						close_hdocs_in_cmd(t_cmd *cmd);
+void						close_all_prepared_hdocs(t_ast *node);
+int							prepare_cmd_heredocs(t_cmd *cmd, t_shell *sh,
+								t_ast *node);
+int							prepare_heredocs(t_ast *node, t_shell *sh);
+int							heredoc_loop(int wfd, t_redir *r, t_shell *sh);
+int							read_heredoc_into_fd(int write_fd, t_redir *redir,
+								t_shell *sh);
 
 #endif
